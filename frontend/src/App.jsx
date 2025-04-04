555ed066a6d8bc6706771815adf1fba7eaa73fda
Say Hoang <140904534+SayHoang@users.noreply.github.com>
2025-04-04 15:55:47 +0000

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useQuery, gql } from "@apollo/client";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Categories from "./components/Categories";
+import Category from "./components/Category";
+import NoPage from "./pages/NoPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Categories />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:id" element={<Category />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
