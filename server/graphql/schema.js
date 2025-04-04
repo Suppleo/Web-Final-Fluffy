bb84adb9cf544c44f1432776f6d2411df334d9ef
Nguyễn Ngọc Thạch <143619931+Suppleo@users.noreply.github.com>
2025-04-04 16:19:20 +0000

    add some server folders

---
+++ server/graphql/schema.js
@@ -0,0 +1,27 @@
+import { createSchema } from 'graphql-yoga';
+import _ from 'lodash';
+
+import { typeDef as tours, resolvers as toursResolvers } from './tours.js';
+import { typeDef as authentication, resolvers as authResolvers } from './authentication.js';
+import { typeDef as uploadTypeDef, resolvers as uploadResolvers } from './upload.js'; // Added upload import
+
+// Base Query and Mutation types
+const query = `
+  type Query {
+    _empty: String
+  }
+
+  type Mutation {
+    _empty: String
+  }
+`;
+
+// const typeDefs = [query, tours, authentication, uploadTypeDef]; // Added uploadTypeDef
+// const resolvers = _.merge({}, toursResolvers, authResolvers, uploadResolvers); // Added uploadResolvers
+
+const schema = createSchema({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+export default schema;
